2f81e3dccf3d1d3d6a9de3a3f015e9c0f84f9e29
Fidaha <hafida.charaf@gmail.com>
2023-07-07 10:02:20 +0200

    "Creation de page de connection"

---
+++ Controllers/LoginController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using VacationSplit.Models;
+
+namespace VacationSplit.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult ProcessLogin(User user)
+        {
+            if (user.Email == "BillGates" && user.Password == "bigbucks") 
+            {
+                return View("LoginSuccess", user);
+            } else 
+            {
+                return View("LoginFailure", user);
+            }
+            
+        }
+    }
+}
