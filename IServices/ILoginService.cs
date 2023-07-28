f48186b3d394867a220646ea1110055768ea9100
Bournane Mohamed <mbournane@gmail.com>
2023-07-28 09:13:45 +0200

    merge dev to feature

---
+++ IServices/ILoginService.cs
@@ -0,0 +1,10 @@
+﻿using VacationSplit.Models;
+
+namespace VacationSplit.IServices
+{
+    public interface ILoginService
+    {
+        public bool FindUserByEmailAndPassword(User user);
+        public bool FindUserByEmail(string email);
+    }
+}
