018d1396354ce8e74a927f998bb9616c5ad9bfde
bouchra171 <bouchramidoune09@gmail.com>
2023-08-07 16:08:13 +0200

    Style Css

--- Controllers/AccountController.cs
+++ Controllers/AccountController.cs
@@ -34,11 +34,16 @@ namespace VacationSplit.Controllers
         {
             using (var context = new VacationSplitContext())
             {
-                User user = context.Users.Where(p => p.Id == id).FirstOrDefault();
+                User user = context.Users.FirstOrDefault(u => u.Id == id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
                 return View(user);
             }
-
         }
+
 
         // GET: AcountController/Create
         public ActionResult Create()
@@ -49,7 +54,7 @@ namespace VacationSplit.Controllers
         // POST: AcountController/Create
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public IActionResult Create(Models.User user, IFormFile file)
+        public IActionResult Create(User user, IFormFile ProfileImg)
         {
             try
             {
@@ -58,60 +63,101 @@ namespace VacationSplit.Controllers
                 {
                     return View();
                 }
-                string uniqueFileName = null;                
-                if (user.ProfileImg.Length > 0)
-                { 
+                string uniqueFileName = null;
+
+                if (ProfileImg != null && ProfileImg.Length > 0)
+                {
                     string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "img");
-                    uniqueFileName = Guid.NewGuid().ToString()+ "-" + user.ProfileImg.FileName;
+                    uniqueFileName = Guid.NewGuid().ToString() + "-" + ProfileImg.FileName;
                     string filePath = Path.Combine(uploadsFolder, uniqueFileName);
                     using (var fileStream = new FileStream(filePath, FileMode.Create))
                     {
-                        user.ProfileImg.CopyTo(fileStream);
-                    }                        
-                }                
+                        ProfileImg.CopyTo(fileStream);
+                    }
+                    user.ProfileImage = "/img/" + uniqueFileName;
+                }
                 string password = user.Password.Trim();
                 user.Password = securityService.Encrypt(password);
-                user.ProfileImage ="/img/"+ uniqueFileName;
                 using (var context = new VacationSplitContext())
                 {
                     context.Add(user);
                     context.SaveChanges();
                 }
-                return View("Details", user);
+                return RedirectToAction("Details", new { id = user.Id });
             }
             catch
             {
                 return View();
             }
         }
+
+
 
         // GET: AcountController/Edit/5
         public ActionResult Edit(int id)
         {
             using (var context = new VacationSplitContext())
             {
-                User user = context.Users.Where(p => p.Id == id).FirstOrDefault();
+                User user = context.Users.FirstOrDefault(u => u.Id == id);
+                if (user == null)
+                {
+                    return NotFound();
+                }
+
                 return View(user);
             }
-
         }
 
-        // POST: AcountController/Edit/5
+
+
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(Models.User user)
+        public IActionResult Edit(int id, Models.User updatedUser, IFormFile ProfileImg)
         {
             try
             {
                 using (var context = new VacationSplitContext())
                 {
-                    SecurityService securityService = new SecurityService();
-                    string password = user.Password.Trim();
-                    user.Password = securityService.Encrypt(password);
-                    context.Update(user);
-                    context.SaveChanges();
-                    return View("Details", context.Users.Where(p => p.Id == user.Id).FirstOrDefault());
+                    var user = context.Users.FirstOrDefault(u => u.Id == id);
+                    if (user == null)
+                    {
+                        return NotFound();
+                    }
 
+                    if (ModelState.IsValid)
+                    {
+                        user.Email = updatedUser.Email;
+                        user.FirstName = updatedUser.FirstName;
+                        user.LastName = updatedUser.LastName;
+                        user.Ville = updatedUser.Ville;
+
+                        SecurityService securityService = new SecurityService();
+                        string password = updatedUser.Password.Trim();
+                        user.Password = securityService.Encrypt(password);
+
+                        if (ProfileImg != null && ProfileImg.Length > 0)
+                        {
+                            string uploadsFolder = Path.Combine(_webHostEnvironment.WebRootPath, "img");
+                            string uniqueFileName = Guid.NewGuid().ToString() + "-" + ProfileImg.FileName;
+                            string filePath = Path.Combine(uploadsFolder, uniqueFileName);
+                            using (var fileStream = new FileStream(filePath, FileMode.Create))
+                            {
+                                ProfileImg.CopyTo(fileStream);
+                            }
+                            user.ProfileImage = "/img/" + uniqueFileName;
+                        }
+
+                        else
+                        {
+                            user.ProfileImage = updatedUser.ProfileImage;
+                        }
+
+                        context.Update(user);
+                        context.SaveChanges();
+                        return RedirectToAction("Details", new { id = user.Id });
+                    }
+
+                    return View(user);
                 }
             }
             catch
@@ -119,6 +165,11 @@ namespace VacationSplit.Controllers
                 return View();
             }
         }
+
+
+
+
+
 
 
         public ActionResult Delete(int id)
